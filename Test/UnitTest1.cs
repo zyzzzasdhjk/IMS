7c3bcfed7e5fe69bafb9ae863e6efb7a4e1eb051
zy <2075221336@qq.com>
2023-10-06 21:21:34 +0800

    测试文件更新

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,18 +1,22 @@
 using IMS.Service.DataBase;
+using IMS.Service.UserServices;
 
 namespace Test;
 public class Tests
 {
+    private IRelationalDataBase _m;
+    
     [SetUp]
     public void Setup()
     {
+        _m = new MysqlDataBase();
     }
 
     [Test]
     public void Test1()
     {
-        IRelationalDataBase m = new MysqlDataBase();
-        m.Init();
+        var u = new UserService(_m);
+        Console.WriteLine(u.InsertUser("123456abc@", "123456abc@"));
         Assert.Pass();
     }
 }

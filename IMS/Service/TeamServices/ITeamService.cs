357bb4fa9dd3505398136757fcbd47a08e24bde0
zy <2075221336@qq.com>
2023-10-08 17:45:52 +0800

    team

--- IMS/Service/TeamServices/ITeamService.cs
+++ IMS/Service/TeamServices/ITeamService.cs
@@ -2,5 +2,45 @@
 
 public interface ITeamService
 {
+    /// <summary>
+    /// 新增成员
+    /// </summary>
+    /// <returns></returns>
+    public bool AddMember();
     
+    /// <summary>
+    /// 删除成员
+    /// </summary>
+    /// <returns></returns>
+    public bool DeleteMember();
+    
+    /// <summary>
+    /// 获取所有成员
+    /// </summary>
+    /// <returns></returns>
+    public bool GetAllMembers();
+    
+    /// <summary>
+    /// 获取成员
+    /// </summary>
+    /// <returns></returns>
+    public bool GetMember();
+
+    /// <summary>
+    /// 增加管理员
+    /// </summary>
+    /// <returns></returns>
+    public bool AddAdministrator();
+    
+    /// <summary>
+    /// 删除管理员
+    /// </summary>
+    /// <returns></returns>
+    public bool DeleteAdministrator();
+    
+    /// <summary>
+    /// 获取所有管理员
+    /// </summary>
+    /// <returns></returns>
+    public bool GetAllAdministrators();
 }
